7a33fec83b458155253df701ab33209911690dd5
Diego Briceño <diegobriceno13@gmail.com>
2021-03-27 15:08:04 +0000

    Registar animal

--- ClinicaVeterinaria/Program.cs
+++ ClinicaVeterinaria/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace ClinicaVeterinaria
 {
@@ -6,7 +7,7 @@ namespace ClinicaVeterinaria
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            RegistarAnimal();
         }
 
         static void Menu()
@@ -16,7 +17,33 @@ namespace ClinicaVeterinaria
 
         static void RegistarAnimal()
         {
+            string nome = "1";
+            string especie = "1";
+            int idade;
+            Género genero;
+            Console.WriteLine("REGISTAR ANIMAL DE ESTIMAÇÃO:");
+            
+            while (nome.All(char.IsDigit))
+            {
+                Console.Write("Insira o nome do animal (não pode conter dígitos): ");
+                nome = Console.ReadLine();
+            }
 
+            while (especie.All(char.IsDigit))
+            {
+                Console.Write("Insira a espécie (não pode conter dígitos): ");
+                especie = Console.ReadLine();
+            }
+
+            Console.Write("Insira a idade (não pode conter letras): ");
+            string aux_Idade = Console.ReadLine();
+            while (int.TryParse(aux_Idade, out idade))
+            {
+                Console.Write("Insira a idade (não pode conter letras): ");
+                aux_Idade = Console.ReadLine();
+            }
+
+            Console.Write("Insira o nome do animal: ");
         }
 
         static void RegistarCliente() { }
