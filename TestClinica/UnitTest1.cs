0c444691bea5bb1d49b526fe00685cd641bfbc48
Ruben Rodrigues <ruben2000@gmail.com>
2021-03-27 11:04:12 +0000

    Setup Projeto

---
+++ TestClinica/UnitTest1.cs
@@ -0,0 +1,20 @@
+using NUnit.Framework;
+using ClinicaVeterinaria;
+using System;
+
+namespace TestClinica
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
